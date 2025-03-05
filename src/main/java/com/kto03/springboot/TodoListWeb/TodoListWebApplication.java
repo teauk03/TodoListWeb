3bbafd11470e37d1f849ff508051aea712af3c68
teauk03 <kto031124@gamil.com>
2025-03-05 09:36:57 +0900

    Test Commit

---
+++ src/main/java/com/kto03/springboot/TodoListWeb/TodoListWebApplication.java
@@ -0,0 +1,13 @@
+package com.kto03.springboot.TodoListWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kto03/springboot/TodoListWeb/TodoListWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kto03.springboot.TodoListWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
