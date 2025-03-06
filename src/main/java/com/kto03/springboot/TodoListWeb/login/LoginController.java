aca9089bd0845540df767072aeb90bd450449b66
teauk03 <kto031124@gamil.com>
2025-03-06 16:31:41 +0900

    로그인 컨트롤러에 @SessionAttribute 추가

--- src/main/java/com/kto03/springboot/TodoListWeb/login/LoginController.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/login/LoginController.java
@@ -5,11 +5,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.kto03.springboot.TodoListWeb.AuthenticationService;
 
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
     private AuthenticationService AuthenticationService;
 
@@ -25,6 +27,9 @@ public class LoginController {
     public String gotoLoginPage() {
         return "login";
     }
+
+
+    
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
         if (AuthenticationService.authenticate(name, password)) {
