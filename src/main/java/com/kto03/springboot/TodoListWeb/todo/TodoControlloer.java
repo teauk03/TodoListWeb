dc6b1103fb354e4abefe6b5ccc67b380ca966d8f
teauk03 <kto031124@gamil.com>
2025-03-06 16:33:12 +0900

    Todo - Controller 정의

--- src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
@@ -1,15 +1,28 @@
 package com.kto03.springboot.TodoListWeb.todo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 
 @Controller
+@SessionAttributes("name")
 public class TodoControlloer {
+    private TodoService todoService;
+
+    public TodoControlloer(TodoService todoService) {
+        super();
+        this.todoService = todoService;
+    }
     
     @RequestMapping("list-todos")
-    public String listAllTodoList() {
+    public String listAllTodoList(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("cho");
+        model.addAttribute("todos",todos);
         return "listTodos";
     }
 }
