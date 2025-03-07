c257c9918e5d4c93cb9b849bbe1c532028777904
teauk03 <kto031124@gamil.com>
2025-03-07 13:31:18 +0900

    Todo 추가 기능 구현

--- src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoService.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoService.java
@@ -9,15 +9,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
     // DB 넣기전 임시 데이터
     static {
-        todos.add(new Todo(1, "cho", "Spring", LocalDate.now().plusDays(1), false));
-        todos.add(new Todo(2, "kti", "C#", LocalDate.now().plusDays(1), false));
-        todos.add(new Todo(3, "chi", "C++", LocalDate.now().plusDays(1), false));
+        todos.add(new Todo(++todosCount, "cho", "Spring", LocalDate.now().plusDays(1), false));
+        todos.add(new Todo(++todosCount, "kti", "C#", LocalDate.now().plusDays(1), false));
+        todos.add(new Todo(++todosCount, "chi", "C++", LocalDate.now().plusDays(1), false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        todos.add(todo);
     }
 }
     
