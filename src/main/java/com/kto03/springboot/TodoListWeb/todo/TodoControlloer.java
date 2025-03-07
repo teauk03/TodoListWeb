f8e38da83d0c7a279fb297c721099a888d51a54e
teauk03 <kto031124@gamil.com>
2025-03-07 13:31:36 +0900

    Todo 추가 기능을 위한 컨트롤러

--- src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
@@ -1,11 +1,13 @@
 package com.kto03.springboot.TodoListWeb.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
@@ -33,7 +35,10 @@ public class TodoControlloer {
     }
 
     @RequestMapping(value="add-todo", method = RequestMethod.POST)
-    public String addNewTodo() {
+    public String addNewTodo(@RequestParam String description, ModelMap model) {
+        String username = (String) model.get("name");
+        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
 }
+
