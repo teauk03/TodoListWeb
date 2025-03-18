fb00d2262084b6f42f5b657277c0ebde30632d88
teauk03 <kto031124@gamil.com>
2025-03-19 08:18:47 +0900

    수정 컨트롤러

--- src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/todo/TodoControlloer.java
@@ -33,7 +33,7 @@ public class TodoControlloer {
         model.addAttribute("todos", todos);
         return "listTodos";
     }
-
+    
     @RequestMapping(value="add-todo", method = RequestMethod.GET)
     public String showNewTodoPage(ModelMap model) {
        String username = (String)model.get("name");
@@ -48,13 +48,33 @@ public class TodoControlloer {
             return "todo";
         }
         String username = (String) model.get("name");
-        todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+        todoService.addTodo(username, todo.getDescription(),todo.getTargetDate(), false);
         return "redirect:list-todos";
     }
-    
+
     @GetMapping("delete-todo")
     public String deleteTodo(@RequestParam int id) {
         todoService.deleteById(id);
         return "redirect:list-todos";
     }
-}
+
+    // update-todo
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET) 
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value="update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if(result.hasErrors()) {
+            return "todo";
+        }
+        String username = (String) model.get("name");
+        todo.setUsername(username);
+        todoService.updateTodo(todo);
+        return "redirect:list-todos";
+    }
+
+} 
