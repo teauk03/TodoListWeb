ed8f736eaa2e1eba9890c7e71c557a6e8e0d091a
teauk03 <kto031124@gamil.com>
2025-03-20 16:04:36 +0900

    Security 설정

---
+++ src/main/java/com/kto03/springboot/TodoListWeb/SpringSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.kto03.springboot.TodoListWeb;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfig {
+    
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails1 = createNewUser("admin", "adminpass");
+        UserDetails userDetails2 = createNewUser("user", "userpass");
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder = 
+        input -> passwordEncoder().encode(input);
+        
+        UserDetails userDetails = User.builder()
+        .passwordEncoder(passwordEncoder)
+        .username(username)
+        .password(password)
+        .roles("USER","ADMIN")
+        .build();
+        return userDetails;
+    }
+
+    @Bean 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
