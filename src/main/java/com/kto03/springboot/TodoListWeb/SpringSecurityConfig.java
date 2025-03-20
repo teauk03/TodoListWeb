2897e7c61b9bb8ba216e950460173881878c982b
teauk03 <kto031124@gamil.com>
2025-03-21 08:56:40 +0900

    Spring Security 설정

--- src/main/java/com/kto03/springboot/TodoListWeb/SpringSecurityConfig.java
+++ src/main/java/com/kto03/springboot/TodoListWeb/SpringSecurityConfig.java
@@ -4,11 +4,15 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -38,5 +42,20 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // 모든 요청에 대해 인증을 요구하도록 설정
+    http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+    // 기본 로그인 폼 활성화
+    http.formLogin(withDefaults());
+    // CSRF 보호 비활성화
+    http.csrf().disable();
+    // X-Frame-Options 비활성화
+    http.headers()
+        .frameOptions()
+        .disable();
 
+    return http.build();
+    }
 }
+
