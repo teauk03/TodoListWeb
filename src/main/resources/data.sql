0c81c2d7344b03975cabff645f93d75430528fb8
teauk03 <kto031124@gamil.com>
2025-03-21 13:33:50 +0900

    data.sql (테스트)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1001, 'user', 'java의 정석 3회 읽고 학습', '2026-01-01', 'false'),
+(1002, 'user', 'Spring의 정석 3회 읽고 학습', '2026-06-01', 'false');
